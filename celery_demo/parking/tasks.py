770fb558e408c54f7bac7c3804424d90d9a4ab07
eltonplima <eltonplima@gmail.com>
2020-07-18 07:16:16 +0100

    Update tasks signature

--- celery_demo/parking/tasks.py
+++ celery_demo/parking/tasks.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 
-import json
-
 from celery import shared_task
 from django.core import serializers
 from django.utils import timezone
@@ -10,14 +8,14 @@ from .models import Parking, Report
 
 
 @shared_task(queue="finish_parking")
-def finish_parking(parking_id: int):
+def finish_parking(parking_id: int, *_, **_):
     ended_at = timezone.now()
     Parking.objects.filter(id=parking_id).update(ended_at=ended_at)
     return {"ended_at": ended_at}
 
 
 @shared_task(queue="parking_report")
-def parking_report():
+def parking_report(*_, **_):
     qs = Parking.objects.all()
     data = serializers.serialize("json", qs)
     Report.objects.create(data=data)
