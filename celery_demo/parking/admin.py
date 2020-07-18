a18478592190af70bfda94e3ca23fcc51a0c37c8
eltonplima <eltonplima@gmail.com>
2020-07-18 07:12:36 +0100

    Update ParkingAdmin
    * Set ended_at as read only

--- celery_demo/parking/admin.py
+++ celery_demo/parking/admin.py
@@ -5,8 +5,8 @@ from .models import Parking, Report
 
 @admin.register(Parking)
 class ParkingAdmin(admin.ModelAdmin):
-    fields = ("plate", "start_at", "end_at")
     list_display = ("plate", "start_at", "end_at", "finished")
+    readonly_fields = ("ended_at",)
 
     def finished(self, obj):
         return obj.ended_at is not None
